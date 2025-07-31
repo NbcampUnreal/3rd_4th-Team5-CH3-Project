6c9bf273c7fcf00638b28a15b83660bee9ea9f8e
Hyeongcheol <gudcjf6543@gmail.com>
2025-07-31 17:56:32 +0900

    Reapply "AI 순찰"
    This reverts commit 4b445b0aa26110c53636e3d3ca84ed9dd6969a55.

--- Source/CH3Project/Private/AI/BaseAIController.cpp
+++ Source/CH3Project/Private/AI/BaseAIController.cpp
@@ -1,5 +1,149 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "AI/BaseAIController.h"
+#include "AI/BaseAICharacter.h"
+#include "NavigationSystem.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Kismet/GameplayStatics.h"
 
+ABaseAIController::ABaseAIController()
+{
+	AIPerception = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerception"));
+	SetPerceptionComponent(*AIPerception);
+	
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	if (SightConfig)
+	{
+		SightConfig->SightRadius = 1500.0f;
+		SightConfig->LoseSightRadius = SightConfig->SightRadius + 50.0f;
+		SightConfig->PeripheralVisionAngleDegrees = 90.0f;
+		SightConfig->SetMaxAge(5.0f);
+		SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+		SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+		SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+		
+		AIPerception->ConfigureSense(*SightConfig);
+		AIPerception->SetDominantSense(SightConfig->GetSenseImplementation());
+	}
+}
+
+void ABaseAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AIPerception)
+	{
+		AIPerception->OnTargetPerceptionUpdated.AddDynamic(this, &ABaseAIController::OnPerceptionUpdate);
+	}
+
+	GetWorldTimerManager().SetTimer(RandomMoveTimer, this, &ABaseAIController::MoveToRandomLocation, 5.0f, true, 1.0f);
+}
+
+
+void ABaseAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	if (InPawn)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller possessed pawn: %s"), *InPawn->GetName());
+	}
+}
+
+
+void ABaseAIController::MoveToRandomLocation()
+{
+	APawn* MyPawn = GetPawn();
+	if (!MyPawn)
+	{
+		return;
+	}
+
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+	if (!NavSystem)
+	{
+		return;
+	}
+
+	FNavLocation RandomLocation;
+
+	bool bFoundLocation = NavSystem->GetRandomPointInNavigableRadius(MyPawn->GetActorLocation(), MoveRadius, RandomLocation);
+
+	if (bFoundLocation)
+	{
+		MoveToLocation(RandomLocation.Location);
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller moving to random location: %s"), *RandomLocation.Location.ToString());
+	}
+
+}
+
+void ABaseAIController::OnPerceptionUpdate(AActor* Actor, FAIStimulus Stimulus)
+{
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	
+	if (Actor != PlayerPawn)
+	{
+		return;
+	}
+
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		DrawDebugString(GetWorld(), Actor->GetActorLocation() + FVector(0, 0, 100), FString::Printf(TEXT("Detected: %s"), *Actor->GetName()), nullptr, FColor::Green, 2.0f, true);
+
+		StartChasingTarget(Actor);
+	}
+	else
+	{
+		DrawDebugString(GetWorld(), Actor->GetActorLocation() + FVector(0, 0, 100), FString::Printf(TEXT("Lost: %s"), *Actor->GetName()), nullptr, FColor::Red, 2.0f, true);
+		
+		StopChasingTarget();
+	}
+}
+
+void ABaseAIController::StartChasingTarget(AActor* Target)
+{
+	if(bIsChasing && TargetActor == Target)
+	{
+		return;
+	}
+
+	TargetActor = Target;
+	bIsChasing = true;
+
+	GetWorldTimerManager().ClearTimer(RandomMoveTimer);
+
+	if(ABaseAICharacter* AIChar = Cast<ABaseAICharacter>(GetPawn()))
+	{
+		AIChar->setMoveSpeed(AIChar -> RunSpeed);
+	}
+
+	UpdateChasingTarget();
+
+	GetWorldTimerManager().SetTimer(ChasingTimer, this, &ABaseAIController::UpdateChasingTarget, 0.25f, true);
+}
+
+
+void ABaseAIController::UpdateChasingTarget()
+{
+	if (TargetActor && bIsChasing)
+	{
+		MoveToActor(TargetActor, 500.0f);
+	}
+}
+
+void ABaseAIController::StopChasingTarget()
+{
+	if (!bIsChasing) return;
+	
+	TargetActor = nullptr;
+		bIsChasing = false;
+		
+	GetWorldTimerManager().ClearTimer(ChasingTimer);
+
+	StopMovement();
+
+	if (ABaseAICharacter* AIChar = Cast<ABaseAICharacter>(GetPawn()))
+	{
+		AIChar->setMoveSpeed(AIChar->WalkSpeed);
+	}
+	
+	GetWorldTimerManager().SetTimer(RandomMoveTimer, this, &ABaseAIController::MoveToRandomLocation, 3.0f, true, 2.0f);
+}
--- Source/CH3Project/Public/AI/BaseAIController.h
+++ Source/CH3Project/Public/AI/BaseAIController.h
@@ -1,17 +1,49 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "BaseAIController.generated.h"
 
-/**
- * 
- */
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+
 UCLASS()
 class CH3PROJECT_API ABaseAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+public:
+	ABaseAIController();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	UAIPerceptionComponent* AIPerception;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	UAISenseConfig_Sight* SightConfig;
+
+	UFUNCTION()
+	void OnPerceptionUpdate(AActor* Actor, FAIStimulus Stimulus);
+
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void BeginPlay() override;
+
+private:
+	void MoveToRandomLocation();
+
+	FTimerHandle RandomMoveTimer;
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float MoveRadius = 1000.0f;
+
+	UPROPERTY()
+	AActor* TargetActor = nullptr;
+
+	bool bIsChasing = false;
+	FTimerHandle ChasingTimer;
+
+	void StartChasingTarget(AActor* Target);
+	void StopChasingTarget();
+	void UpdateChasingTarget();
+
 };
