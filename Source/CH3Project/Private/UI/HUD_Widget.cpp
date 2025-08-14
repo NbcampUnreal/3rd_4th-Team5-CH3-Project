93d35bb03f6eee33134761753f41aa46526a7162
sumin2391 <baesumin0211@gmail.com>
2025-08-14 13:32:31 +0900

    UI 최종 변경사항 완료

--- Source/CH3Project/Private/UI/HUD_Widget.cpp
+++ Source/CH3Project/Private/UI/HUD_Widget.cpp
@@ -1,5 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UI/HUD_Widget.h"
-
+#include "Components/TextBlock.h"
+/*
+void UHUD_Widget::SetTextHUD_Timer(FText Time)
+{
+	HUD_Timer->SetText(Time);
+}
+*/
